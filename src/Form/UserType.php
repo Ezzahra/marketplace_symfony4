6849dfd3186e7995bf7c53b9ae164d0f56d6756e
fatimaezzahra <fatimaezzahraelharradi@gmail.com>
2021-04-08 20:14:58 +0100

    add register feature

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +17,31 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
-            ->add('password')
-        ;
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
+            ->add('roles', ChoiceType::class, array(
+                'choices' => array(
+                    'Achteur' => 'ROLE_BUYER',
+                    'Vendeur' => 'ROLE_SELLER'
+                ),
+                'required' => true,
+                ));
+
+                //roles field data transformer
+        $builder->get('roles')
+        ->addModelTransformer(new CallbackTransformer(
+            function ($rolesArray) {
+                // transform the array to a string
+                return count($rolesArray)? $rolesArray[0]: null;
+            },
+            function ($rolesString) {
+                // transform the string back to an array
+                return [$rolesString];
+    }
+));
     }
 
     public function configureOptions(OptionsResolver $resolver)
